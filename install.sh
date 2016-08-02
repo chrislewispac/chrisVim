c8966da997b8d929d023fdff79b5eded15ee21b7
Chris Lewis <chrislewis@golfingbuddha.com>
2016-08-02 19:12:30 -0400

    install solarized and pathogen in sh script

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 # My first script
 
 cp ./vim_settings/.vimrc ~/
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+
